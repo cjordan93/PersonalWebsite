f65b9c75cb55ac73bd5d10ce52a13957a7f3cafb
Christopher Jordan <christopherjordan@ChristophersMBP.home>
2015-11-03 18:56:12 -0500

    Completed resume page and jquery functionality

--- js/main.js
+++ js/main.js
@@ -7,5 +7,15 @@ $(document).ready(function() {
     $('.fa-bars').toggle();
     $('.fa-times').toggle();
   });
+
+  /* Resume Navigation - on click, show the content with the same index and hide the siblings. Change the opacity   of the selected navigation to 1 and set the siblings to 0.5. */
+  $('.resume-content .content').hide();
+
+  $('.resume-nav li').on('click',function(e) {
+    e.preventDefault();
+    var resumeContent = $(this).index();
+    $('.content').eq(resumeContent).fadeToggle(600).show().siblings().hide();
+    $(this).css('opacity', 1).siblings().css('opacity', .5);
+  });
   
 });
